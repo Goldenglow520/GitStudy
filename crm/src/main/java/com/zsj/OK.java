7652ead1f96af76129a4db5cd40ae93acf75fd2a
GoldenGlow <654414824@qq.com>
2022-10-13 15:44:42 +0800

    commit第二次提交

---
+++ crm/src/main/java/com/zsj/OK.java
@@ -0,0 +1,11 @@
+package com.zsj;/*
+ * @author : Goldenglow
+ * @date   :2022/10/13 15:42
+ * @function:
+ */
+
+public class OK {
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
