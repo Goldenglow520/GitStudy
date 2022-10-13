778f867005d78aebad50df5aa08904230cbb6001
GoldenGlow <654414824@qq.com>
2022-10-13 15:38:15 +0800

    Helo pom

---
+++ crm/src/main/java/com/zsj/Hi.java
@@ -0,0 +1,11 @@
+package com.zsj;/*
+ * @author : Goldenglow
+ * @date   :2022/10/13 15:35
+ * @function:
+ */
+
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
